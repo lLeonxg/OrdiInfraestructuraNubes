524d48fbb82fffb3c86e83805eda0457718959bb
Leo <llxicotencatlg@hotmail.com>
2024-12-04 17:52:14 -0600

    4 commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Ruta para el endpoint GET
 @app.route('/saludo', methods=['GET'])
 def saludo():
-    return jsonify({'mensaje': 'holi prof'}), 200
+    return jsonify({'mensaje': 'hola buenas tardes'}), 200
 
 # Ejecución de la app
 if __name__ == '__main__':
